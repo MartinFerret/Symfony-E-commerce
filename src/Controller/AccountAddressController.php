7597d94342d8ff3d2305eff4ffd562267a2e2c0f
Martin Ferret <ferretmartin91@gmail.com>
2022-03-05 22:23:57 +0100

    modifications ok

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Address;
 use App\Form\AddressType;
+use App\Repository\AddressRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -47,5 +49,48 @@ class AccountAddressController extends AbstractController
         return $this->renderForm('account/address_add.html.twig', [
             "form" => $form
         ]);
+    }
+
+    /**
+     * @Route("/account/address/edit/{id}", name="address_edit")
+     */
+    public function edit(Request $request, EntityManagerInterface $doctrine, int $id, AddressRepository $ar): Response
+    {
+
+        $address = $ar->find($id); 
+     
+        if (!$address || $address->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('account');
+        }
+
+        $form = $this->createForm(AddressType::class, $address);
+     
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $doctrine->flush();
+
+            return $this->redirectToRoute('account');
+        }
+        return $this->renderForm('account/address_add.html.twig', [
+            "form" => $form
+        ]);
+    }
+
+    /**
+     * @Route("/account/address/delete/{id}", name="address_delete")
+     */
+    public function delete(Request $request, EntityManagerInterface $doctrine, int $id, AddressRepository $ar): Response
+    {
+
+        $address = $ar->find($id); 
+     
+        if ($address && $address->getUser() == $this->getUser()) {
+            $doctrine->remove($address);
+            $doctrine->flush();
+        }
+
+        return $this->redirectToRoute('account_address');
     }
 }
