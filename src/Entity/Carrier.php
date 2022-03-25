292087d3b7c8b7d1e31ec97e24dd4721e355f5b2
Martin Ferret <ferretmartin91@gmail.com>
2022-03-25 22:46:17 +0100

    carrier form ok

--- src/Entity/Carrier.php
+++ src/Entity/Carrier.php
@@ -72,4 +72,9 @@ class Carrier
 
         return $this;
     }
+
+    public function __toString(): ?string
+    {
+        return $this->getName() . "[br]" . $this->getDescription() . "[br]" . number_format($this->getPrice(), 2, ',', '' ) . " euros";
+    }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Address;
+use App\Entity\Carrier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,6 +21,13 @@ class OrderType extends AbstractType
                 'required' => true,
                 "class" => Address::class,
                 'choices'=> $user->getAddresses(),
+                "multiple" =>  false,
+                "expanded" => true
+            ])
+            ->add('carriers', EntityType::class, [
+                "label" => "Choose your carrier",
+                'required' => true,
+                "class" => Carrier::class,
                 "multiple" =>  false,
                 "expanded" => true
             ])
